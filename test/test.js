baddbe3712dea4434ec93f3dec44c0abc5602e01
jennyliang <jiaojiaomao220@163.com>
2022-11-16 10:59:28 +0800

    test2

--- test/test.js
+++ test/test.js
@@ -376,7 +376,7 @@ describe('SpecberusWrapper', () => {
     const version = SpecberusWrapper.version();
     // it('should be a format of semantic versioning', () => {
     //   expect(version).to.eventually.match(/\d+.\d+.\d+/);
-    // });
+    });
   });
 
   describe('validate(url)', () => {
